89182b4df44af0258a7bb3b600cd77cf20570ede
gosaviharshali <gosaviharshali123@example.com>
2022-12-08 07:53:54 -0800

    Add new assignment

---
+++ src/com/assignment1/Training_center.java
@@ -0,0 +1,9 @@
+package com.assignment1;
+
+public class Training_center {
+  public static void main(String [] args) {
+	  
+          System.out.println("Welcome in pune training center");
+  }
+ 
+}
